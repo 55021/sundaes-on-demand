555274440387522bbb071f7313a0995ca5231338
MariaRitaSantana <mariarita.santana@mercadolivre.com>
2023-01-05 22:23:09 -0300

    5/6 complete

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,24 +2,38 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import ScoopOption from './ScoopOption';
 import Row from 'react-bootstrap/Row';
+import ToppingOption from './ToppingOption';
+import Alert from 'react-bootstrap/Alert';
 
 export default function Options({ optionType }) {
     const [items, setItems] = useState([]);
+    const [err, setErr] = useState(false);
 
     useEffect(() => {
         axios.get(`http://localhost:3030/${optionType}`)
-           .then(res => setItems(res.data))
-           .catch(err => console.log(err));
-    }, [optionType]);
+           .then(res => {
+               setItems(res.data);
+               if (err) {
+                   setErr(false);
+               }
+        })
+           .catch(err => setErr(true));
+    }, [optionType, err]);
 
-    const ItemComponent = optionType === 'scoops' ? ScoopOption : '';
+    const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
     const optionItems = items.map(({name, imagePath}) => (
     <ItemComponent key={name} name={name} imagePath={imagePath} />
     ));
 
+    const alertBanner = (
+        <Alert variant="danger">
+            An unexpected error occurred. Please try again later.
+        </Alert>
+    )
+
     return (
     <Row>
-        {optionItems}
+        !err ? {optionItems} : {alertBanner}
     </Row>);
 };
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -11,4 +11,23 @@ test('displays image for each scoop option from server', async () => {
     
     const altText = scoopImages.map(scoop => scoop.alt);
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays image for each topping option from server', async () => {
+    render(<Options optionType="toppings" />);
+
+    const toppingImages = await screen.findAllByRole('img', { name: /topping$/i });
+
+    expect(toppingImages).toHaveLength(3);
+
+    const toppingTitles = toppingImages.map(topping => topping.alt);
+    expect(toppingTitles).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping']);
+})
+
+test('displays alertBanner for error', async() => {
+    render(<Options optionType="juggler" />);
+
+    const alertBanner = await screen.findByRole('alert');
+
+    expect(alertBanner).toBeInTheDocument();
 })
